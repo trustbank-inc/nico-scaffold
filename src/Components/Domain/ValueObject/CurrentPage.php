5a7e77e3bfa272f9dd51333cdc711b986a9afdd3
youhei-ushio <youhei@sea-salt.jp>
2021-08-09 04:43:43 +0900

    一覧パラメータの件数とページの値オブジェクトを定義

---
+++ src/Components/Domain/ValueObject/CurrentPage.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Seasalt\NicoScaffold\Components\Domain\ValueObject;
+
+/**
+ * 1ページあたりの件数
+ */
+final class CurrentPage
+{
+    use IntegerValue {
+        fromString as private _fromString;
+    }
+
+    public static function getMinValue(): int
+    {
+        return 1;
+    }
+
+    /**
+     * 文字列からオブジェクトを生成する
+     *
+     * @param string $value
+     * @return self
+     */
+    public static function fromString(string $value): self
+    {
+        if ($value === '') {
+            return self::fromNumber(self::getMinValue());
+        }
+        return self::_fromString($value);
+    }
+}
---
+++ src/Components/Domain/ValueObject/LimitPerPage.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Seasalt\NicoScaffold\Components\Domain\ValueObject;
+
+/**
+ * 1ページあたりの件数
+ */
+final class LimitPerPage
+{
+    use IntegerValue {
+        fromString as private _fromString;
+    }
+
+    public static function getMinValue(): int
+    {
+        return 1;
+    }
+
+    public static function getMaxValue(): int
+    {
+        return 1000;
+    }
+
+    /**
+     * 文字列からオブジェクトを生成する
+     *
+     * @param string $value
+     * @return self
+     */
+    public static function fromString(string $value): self
+    {
+        if ($value === '') {
+            return self::fromNumber(self::getMaxValue());
+        }
+        return self::_fromString($value);
+    }
+}
