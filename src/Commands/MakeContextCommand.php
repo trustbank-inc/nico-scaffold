b38e7b902526d403baa43668b3404cd704a6bd1d
Makoto Ishihara <89056270+ishihara-makoto-trustbank@users.noreply.github.com>
2021-09-11 17:07:26 +0900

    add: actionを指定し欲しいコンポーネントのみ取得できる機能を追加

--- src/Commands/MakeContextCommand.php
+++ src/Commands/MakeContextCommand.php
@@ -8,14 +8,25 @@ use Illuminate\Console\Command;
 final class MakeContextCommand extends Command
 {
     /**
+     * action: index, detail, create, store, update, destroy, common and all
+     *      common: entity and repository only
+     *      all   : all components
+     * 
      * @var string
      */
-    protected $signature = 'make:context {name} {entity} {model?} {--api}';
+    protected $signature = 'make:context {name} {entity} {model?}
+        {--action: index, detail, create, store, update, destroy, common and all (default: all)} {--api}';
 
     /**
      * @var string
      */
     protected $description = 'Create a new context';
+
+    /** @var array */
+    private $apiParam;
+
+    /** @var array */
+    private $apiTestParam;
 
     /**
      * @return int
@@ -25,56 +36,151 @@ final class MakeContextCommand extends Command
         $context = $this->argument('name');
         $entity = $this->argument('entity');
         $model = $this->argument('model') ?? $this->argument('entity');
+        $action = $this->option('action') ?? 'all';
 
-        $this->call('make:entity', compact('context', 'entity'));
-        $this->call('make:repository-exception', compact('context', 'entity'));
-        $this->call('make:repository-model', compact('context', 'model'));
-        $this->call('make:repository-model-factory', compact('model'));
-        $this->call('make:interactor-store', compact('context', 'entity'));
-        $this->call('make:interactor-index', compact('context', 'entity'));
-        $this->call('make:interactor-detail', compact('context', 'entity'));
-        $this->call('make:interactor-update', compact('context', 'entity'));
-        $this->call('make:interactor-destroy', compact('context', 'entity'));
-        $this->call('make:interactor-store-test', compact('context', 'entity', 'model'));
-        $this->call('make:interactor-index-test', compact('context', 'entity', 'model'));
-        $this->call('make:interactor-detail-test', compact('context', 'entity', 'model'));
-        $this->call('make:interactor-update-test', compact('context', 'entity', 'model'));
-        $this->call('make:interactor-destroy-test', compact('context', 'entity', 'model'));
         if ($this->option('api')) {
-        	$apiParam = [
+            $this->apiParam = [
 				'context' => $context,
 				'entity' => $entity,
 				'--api' => true,
 			];
-			$this->call('make:controller-index', $apiParam);
-			$this->call('make:controller-detail', $apiParam);
-			$this->call('make:controller-store', $apiParam);
-			$this->call('make:controller-update', $apiParam);
-			$this->call('make:controller-destroy', $apiParam);
-			$apiTestParam = [
+            $this->apiTestParam = [
 				'context' => $context,
 				'entity' => $entity,
 				'model' => $model,
 			];
-			$this->call('make:api-test-index', $apiTestParam);
-			$this->call('make:api-test-detail', $apiTestParam);
-			$this->call('make:api-test-store', $apiTestParam);
-			$this->call('make:api-test-update', $apiTestParam);
-			$this->call('make:api-test-destroy', $apiTestParam);
-            $this->call('make:api-token-migration');
-		} else {
-			$this->call('make:controller-index', compact('context', 'entity'));
-			$this->call('make:controller-create', compact('context', 'entity'));
-			$this->call('make:controller-store', compact('context', 'entity'));
-			$this->call('make:controller-detail', compact('context', 'entity'));
-			$this->call('make:controller-update', compact('context', 'entity'));
-			$this->call('make:controller-destroy', compact('context', 'entity'));
-			$this->call('make:view-index', compact('context', 'entity'));
-			$this->call('make:view-create', compact('context', 'entity'));
-			$this->call('make:view-detail', compact('context', 'entity'));
-		}
+        }
 
+        switch ($action) {
+            case 'all':
+                $this->makeAllComponents();
+                break;
+            case 'common':
+                $this->makeCommonComponents();
+                break;
+            case 'index':
+                $this->makeCommonComponents();
+                $this->makeIndexComponents();
+                break;
+            case 'detail':
+                $this->makeCommonComponents();
+                $this->makeDetailComponents();
+                break;
+            case 'create':
+                $this->makeCommonComponents();
+                $this->makeCreateComponents();
+                break;
+            case 'store':
+                $this->makeCommonComponents();
+                $this->makeStoreComponents();
+                break;
+            case 'update':
+                $this->makeCommonComponents();
+                $this->makeUpdateComponents();
+                break;
+            case 'destroy':
+                $this->makeCommonComponents();
+                $this->makeDestroyComponents();
+                break;
+            default:
+                $this->line('<info>Context classes\'s creation faild.</info>');
+        }
         $this->line('<info>Context classes created successfully.</info>');
-        return 0;
+        return 0;  
+    }
+
+    public function makeAllComponents():void
+    {
+        $this->makeCommonComponents();
+        $this->makeIndexComponents();
+        $this->makeDetailComponents();
+        $this->makeCreateComponents();
+        $this->makeStoreComponents();
+        $this->makeUpdateComponents();
+        $this->makeDestroyComponents();
+    }
+
+    public function makeCommonComponents(): void
+    {
+        $this->call('make:entity', compact('context', 'entity'));
+        $this->call('make:repository-exception', compact('context', 'entity'));
+        $this->call('make:repository-model', compact('context', 'model'));
+        $this->call('make:repository-model-factory', compact('model'));
+        if ($this->option('api')) {
+            $this->call('make:api-token-migration');
+        }
+    }
+
+    public function makeIndexComponents(): void
+    {
+        $this->call('make:interactor-index', compact('context', 'entity'));
+        $this->call('make:interactor-index-test', compact('context', 'entity', 'model'));
+        if ($this->option('api')) {
+		    $this->call('make:controller-index', $this->apiParam);
+		    $this->call('make:api-test-index', $this->apiTestParam);
+        } else {
+            $this->call('make:controller-index', compact('context', 'entity'));
+            $this->call('make:view-index', compact('context', 'entity'));
+        }
+    }
+
+    public function makeDetailComponents(): void
+    {
+        $this->call('make:interactor-detail', compact('context', 'entity'));
+        $this->call('make:interactor-detail-test', compact('context', 'entity', 'model'));
+        if ($this->option('api')) {
+            $this->call('make:controller-detail', $this->apiParam);
+            $this->call('make:api-test-detail', $this->apiTestParam);
+        } else {
+            $this->call('make:controller-detail', compact('context', 'entity'));
+            $this->call('make:view-detail', compact('context', 'entity'));
+        }
+    }
+
+    public function makeCreateComponents(): void
+    {
+        if ($this->option('api')) {
+
+        } else {
+		    $this->call('make:controller-create', compact('context', 'entity'));
+		    $this->call('make:view-create', compact('context', 'entity'));
+        }
+    }
+
+    public function makeStoreComponents(): void
+    {
+        $this->call('make:interactor-store', compact('context', 'entity'));
+        $this->call('make:interactor-store-test', compact('context', 'entity', 'model'));
+        if ($this->option('api')) {
+            $this->call('make:controller-store', $this->apiParam);
+            $this->call('make:api-test-store', $this->apiTestParam);
+        } else {
+            $this->call('make:controller-store', compact('context', 'entity'));
+        }
+
+    }
+
+    public function makeUpdateComponents(): void
+    {
+        $this->call('make:interactor-update', compact('context', 'entity'));
+        $this->call('make:interactor-update-test', compact('context', 'entity', 'model'));
+        if ($this->option('api')) {
+            $this->call('make:controller-update', $this->apiParam);
+            $this->call('make:api-test-update', $this->apiTestParam);
+        } else {
+		    $this->call('make:controller-update', compact('context', 'entity'));
+        }
+    }
+
+    public function makeDestroyComponents(): void
+    {
+        $this->call('make:interactor-destroy', compact('context', 'entity'));
+        $this->call('make:interactor-destroy-test', compact('context', 'entity', 'model'));
+        if ($this->option('api')) {
+            $this->call('make:controller-destroy', $this->apiParam);
+            $this->call('make:api-test-destroy', $this->apiTestParam);
+        } else {
+            $this->call('make:controller-destroy', compact('context', 'entity'));
+        }
     }
 }
